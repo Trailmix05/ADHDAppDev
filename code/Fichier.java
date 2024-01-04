209601790dac8805719437feb16a52c598583907
Trailmix05 <126606406+Trailmix05@users.noreply.github.com>
2024-01-04 19:00:21 +0100

    fichier

---
+++ code/Fichier.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+
+public class Fichier {
+    public static void save(){
+        File saveFile = new File("information.txt");
+        
+    }
+}
